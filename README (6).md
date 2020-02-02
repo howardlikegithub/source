e7b6c2dbcfb3422e2bc590097ac780c66be3cb41
howardlikegithub <38270922+howardlikegithub@users.noreply.github.com>
2020-02-02 14:27:03 +0800

    shairf-new-jqeury

---
+++ README (6).md
@@ -0,0 +1,21 @@
+Brackets New HTML5 document
+===============
+
+When double-clicked, a sidebar or toolbar to create a new Html5 document.
+
+
+Run
+
+    File -> 'New Html5...'
+    or
+    Sidebar double-click
+    or 
+    Toolbar double-click
+
+
+![Bracekt html5](http://i62.tinypic.com/1564fow.png)
+
+Yasin Kuyu - [Insya](http://insya.com)
+
+License
+MIT
---
+++ main (7).js
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, window, $, Mustache, navigator */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var docIndex         = 1;
+    var commandId        = "sharif.newdoc.cmd";
+
+    // Menu
+    var html5File        = "sharif.newhtml5";
+    var html5FileLabel   = "New jQuery Document!";
+
+    // Brackets modules
+    var DocumentManager             = brackets.getModule("document/DocumentManager"),
+        Commands                    = brackets.getModule("command/Commands"),
+        CommandManager              = brackets.getModule("command/CommandManager"),
+        KeyBindingManager           = brackets.getModule("command/KeyBindingManager"),
+        EditorManager               = brackets.getModule("editor/EditorManager"),
+        MainViewManager             = brackets.getModule("view/MainViewManager"),
+        Menus                       = brackets.getModule("command/Menus");
+
+    // Template file
+    var Html5Template               = require("text!htmlContent/new_jquery.html");
+
+    // Brackets elements
+    var sidebar = $("#sidebar"),
+        toolbar = $("#main-toolbar");
+
+    /**
+     * Create a new untitled document in the workingset, and make it the current document.
+     * Promise is resolved (synchronously) with the newly-created Document.
+     */
+    function handleFileNew() {
+
+        var defaultExtension = ".html";  // disable preference setting for now
+
+        var doc = DocumentManager.createUntitledDocument(docIndex++, defaultExtension);
+        MainViewManager._edit(MainViewManager.ACTIVE_PANE, doc);
+        
+        // Set template
+        handleTempate(Html5Template);
+        
+        return new $.Deferred().resolve(doc).promise();
+       
+    }
+    
+    function handleTempate(templateContent)
+    {
+        
+        try
+        {
+            
+           // Detect active editor
+           var activeEditor = EditorManager.getActiveEditor();
+            
+               // Set template content
+               activeEditor.document.replaceRange(templateContent, activeEditor.getCursorPos());
+        }
+        catch(err){}
+       
+    }
+    
+    // Sidebar double-click event
+    sidebar.on('dblclick', 'div', function(e) {
+
+        // if not children element
+        if (e.target === this) {
+            handleFileNew();
+        }
+
+    });
+
+    // Toolbar double-click event
+    toolbar.on('dblclick', function(e) {
+
+        // if not children element
+        if (e.target === this) {
+            handleFileNew();
+        }
+
+    });
+
+    // Register command
+    CommandManager.register(html5FileLabel, html5File, handleFileNew);
+
+    // Set menu
+    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    menu.addMenuItem(html5File, undefined, Menus.AFTER, Commands.FILE_NEW_UNTITLED);
+
+    // Shortcut add binding
+    KeyBindingManager.addBinding(commandId, "Ctrl-Alt-N", "mac");
+    KeyBindingManager.addBinding(commandId, "Ctrl-Alt-N", "win");
+
+
+});
